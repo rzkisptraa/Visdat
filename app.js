9ee9c63327c975410e116b77e8f775eb299fcb5e
fupask <mentpaskahbuant@gmail.com>
2026-06-15 12:46:05 +0700

    feat: simplify relativeChart legend onClick handler to use native Chart.js transitions without full-wrapper blinking

--- app.js
+++ app.js
@@ -893,36 +893,17 @@ function renderRelativeChart() {
                     onClick: function(e, legendItem, legend) {
                         const index = legendItem.datasetIndex;
                         const ci = legend.chart;
-                        const canvas = ci.canvas;
-                        const wrapper = canvas ? canvas.closest('.chart-container') : null;
 
-                        if (wrapper) {
-                            wrapper.classList.remove('chart-tf-enter');
-                            wrapper.classList.add('chart-tf-exit');
+                        if (ci.isDatasetVisible(index)) {
+                            ci.hide(index);
+                            legendItem.hidden = true;
+                        } else {
+                            ci.show(index);
+                            legendItem.hidden = false;
                         }
 
-                        setTimeout(() => {
-                            if (ci.isDatasetVisible(index)) {
-                                ci.hide(index);
-                                legendItem.hidden = true;
-                            } else {
-                                ci.show(index);
-                                legendItem.hidden = false;
-                            }
-
-                            autoScaleY(ci);
-                            ci.update('none');
-
-                            if (wrapper) {
-                                wrapper.classList.remove('chart-tf-exit');
-                                void wrapper.offsetWidth;
-                                wrapper.classList.add('chart-tf-enter');
-                            }
-
-                            setTimeout(() => {
-                                if (wrapper) wrapper.classList.remove('chart-tf-enter');
-                            }, 380);
-                        }, 190);
+                        autoScaleY(ci);
+                        ci.update();
                     }
                 },
                 tooltip: {
